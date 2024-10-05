6fc67094f2eb89f4ccdcb80b8bc46b49d25005b9
Kamal Malik <kamalmalik.krw@gmail.com>
2024-10-05 16:20:36 +0530

    successfully detected the shapes but it need more optimization

---
+++ code/uas7.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as ny
+
+
+# Load image
+img = cv2.imread('5.png')
+
+
+blur1 = cv2.GaussianBlur(img,(5,5),0)
+filter = cv2.bilateralFilter(blur1,8,55,55)
+edges= cv2.Canny(filter,50,200)
+_, thresh1 = cv2.threshold(edges, 80,255 ,cv2.THRESH_BINARY)
+
+
+
+
+finalimg= cv2.blur(thresh1,(5,5))
+_, thresh2 = cv2.threshold(finalimg, 22,255 ,cv2.THRESH_BINARY)
+
+filter2 = cv2.bilateralFilter(thresh2,11,55,55)
+
+
+
+
+# Find contours
+
+contours, _ = cv2.findContours(filter2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Iterate through contours
+
+
+for contour in contours:
+    # Approximate polygon
+    ep = 0.03* cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, ep , True)
+  
+    # Check if triangle (3 sides)
+    if len(approx) == 3:
+        # Draw triangle
+        cv2.drawContours(img, [contour], -1, (0, 255, 0), 5)
+
+
+
+
+cv2.imshow('Triangle Detection', img)
+cv2.imshow('Triangle Detectn', filter2)
+
+
+
+key =cv2.waitKey(0)
+if key == ord('d'):
+   cv2.destroyAllWindows()
+      
+
+
+# # *************************************************************************
+
+# import cv2
+# import numpy as np
+
+# # Load image
+# img = cv2.imread('7.png')
+
+# # Apply Canny edge detection
+# edges = cv2.Canny(img, 50, 220)
+
+# # Apply thresholding
+# _, thresh = cv2.threshold(edges, 80, 255, cv2.THRESH_BINARY)
+
+# ############################################################
+# ############################################################
+
+# # Apply closing operation to fill gaps
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+# closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+# # Apply erosion to remove noise
+# eroded = cv2.erode(closed, kernel, iterations=1)
+
+# ############################################################
+# ############################################################
+
+# # Find contours
+# contours, _ = cv2.findContours(eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# # Iterate through contours
+# for contour in contours:
+#     # Approximate polygon
+#     epsilon = 0.04 * cv2.arcLength(contour, True)
+#     approx = cv2.approxPolyDP(contour, epsilon, True)
+    
+#     # Check if triangle (3 sides)
+#     if len(approx) == 3:
+#         # Draw triangle
+#         cv2.drawContours(img, [contour], -1, (0, 255, 0), 5)
+
+# # Display the image with detected triangles
+# cv2.imshow('Triangle Detection', img)
+# cv2.imshow('Triangle Detecon', edges)
+# cv2.imshow('Triangle Dettion', thresh)
+# cv2.imshow('Triangle Dection', kernel)
+# cv2.imshow('Triangleetection', closed)
+# cv2.imshow('Triang Detetion', eroded)
+
+
+
+
+# key =cv2.waitKey(0)
+# if key == ord('d'):
+#    cv2.destroyAllWindows()
